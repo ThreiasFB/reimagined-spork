c78815acbbddd2a1a9222da5a8db7432e5016f7a
ThreiasFB <genokiller22@gmail.com>
2019-05-22 19:22:04 -0300

    first commit

---
+++ Ejercicio 16.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+   int a, b, n;
+   cout<<"Ingresar primer numero"<<end1;
+}
---
+++ ej_16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+   int a, b, n, i=0;
+   cout << "Ingresar cantidad de multiplos"<<endl;
+   cin >> n;
+   cout << "Ingresar de que debe ser multiplo"<<endl;
+   cin >> a;
+   cout << "Ingresar de que no debe ser multiplo"<<endl;
+   cin >> b;
+
+   while (i<n){
+//Pueden aparecer 3 resultados distintos: Ser multiplo de a, ser multiplo de b
+// o no ser multiplo de a o b
+   	if(
+   }
+}
